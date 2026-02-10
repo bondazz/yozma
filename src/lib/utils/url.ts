7946d54cc21191aec9e17ad08d81bc83d551edc3
Samir Miriyev <samir.m@Mjfinance.local>
2026-02-10 09:38:21 +0400

    fix: enforce production domain https://bond.az in SEO files

--- src/lib/utils/url.ts
+++ src/lib/utils/url.ts
@@ -1,4 +1,5 @@
 export const getBaseUrl = () => {
+    if (process.env.NODE_ENV === 'production') return 'https://bond.az';
     if (typeof window !== 'undefined') return window.location.origin;
     if (process.env.NEXT_PUBLIC_SITE_URL) return process.env.NEXT_PUBLIC_SITE_URL;
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
